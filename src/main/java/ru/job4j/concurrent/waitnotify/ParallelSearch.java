a8aafee4350130da9ddb330e38fbfa2bd370ef87
AlexeyEsipov <a_esipov@list.ru>
2021-07-26 16:09:42 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/concurrent/waitnotify/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent.waitnotify;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(15);
+        final Thread consumer = new Thread(
+            () -> {
+                while (true) {
+                    try {
+                        if (Thread.currentThread().isInterrupted()) {
+                        break;
+                    }
+                        System.out.println(queue.poll());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        );
+        consumer.start();
+        new Thread(
+            () -> {
+                for (int index = 0; index != 3; index++) {
+                    try {
+                        queue.offer(index);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        ).start();
+        consumer.interrupt();
+    }
+}
