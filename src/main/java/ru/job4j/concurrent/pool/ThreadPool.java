dacbf452d087158823d20cb6df21d40d5be4b988
AlexeyEsipov <a_esipov@list.ru>
2021-08-12 14:57:00 +0300

    1. Реализовать ThreadPool [#1099 #184525]

--- src/main/java/ru/job4j/concurrent/pool/ThreadPool.java
+++ src/main/java/ru/job4j/concurrent/pool/ThreadPool.java
@@ -47,4 +47,19 @@ public class ThreadPool {
             }
         }
     }
+
+    public static void main(String[] args)  {
+        ThreadPool pool = new ThreadPool();
+
+        for (int i = 0; i < 15; i++) {
+            Task task = new Task(i);
+            pool.work(task);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pool.shutdown();
+    }
 }
