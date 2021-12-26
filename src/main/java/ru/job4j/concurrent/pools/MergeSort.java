0cb7ebf3ad2a582a85deac259925f63436f737fb
AlexeyEsipov <a_esipov@list.ru>
2021-12-26 16:25:49 +0300

    Исправления однострочных комментариев

--- src/main/java/ru/job4j/concurrent/pools/MergeSort.java
+++ src/main/java/ru/job4j/concurrent/pools/MergeSort.java
@@ -1,29 +1,7 @@
 package ru.job4j.concurrent.pools;
 
 public class MergeSort {
-/**  public static int[] sort(int[] array) {
-         return sort(array, 0, array.length - 1);
-     }
-
-    private static int[] sort(int[] array, int from, int to) {
-        // при следующем условии, массив из одного элемента
-        // делить нечего, возвращаем элемент
-        if (from == to) {
-            return new int[] {array[from]};
-        }
-        // попали сюда, значит в массиве более одного элемента
-        // находим середину
-        int mid = (from + to) / 2;
-        // объединяем отсортированные части
-        return merge(
-                // сортируем левую часть
-                sort(array, from, mid),
-                // сортируем правую часть
-                sort(array, mid + 1, to)
-        );
-   }
-
-     Метод объединения двух отсортированных массивов*/
+/**  Метод объединения двух отсортированных массивов*/
     public static int[] merge(int[] left, int[] right) {
         int li = 0;
         int ri = 0;
